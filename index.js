4903ec5f240b68010002ddd064bbf2870d47e982
Andrew Bliss <andrewgbliss@gmail.com>
2018-01-04 08:14:06 -0700

    refactor takes in array now

--- index.js
+++ index.js
@@ -3,31 +3,43 @@ const _ = require('lodash');
 
 /**
  * Replace any variables that appear in an auth flow object
- * @param {*} obj1 The object to replace variables
- * @param {*} obj2 The object to look in that has the variables
+ * @param {object} obj1 The object to replace variables
+ * @param {object} store The object to look in that has the variables
  * @param {*} prefix The prefix of what variables to replace
  */
-function replaceVariables(obj1, obj2, prefix) {
+function replaceFromStore(obj, store, prefix) {
   const regex = new RegExp(`\{\{${prefix}\.(.*?)\}\}`, 'g');
-  const obj1Copy = _.cloneDeep(obj1);
-  _.each(deepKeys(obj1Copy), (path) => {
-    let value = _.get(obj1Copy, path);
+  _.each(deepKeys(obj), (path) => {
+    let value = _.get(obj, path);
     if (_.isString(value)) {
       let match;
       while ((match = regex.exec(value)) !== null) {
         const varName = _.get(match, '1');
-        const valueToReplace = _.get(obj2, varName);
+        const valueToReplace = _.get(store, varName);
         if (_.isObject(valueToReplace)) {
-          _.set(obj1Copy, path, valueToReplace);
+          _.set(obj, path, valueToReplace);
         } else {
           const regex2 = new RegExp(`\{\{${prefix}\.${varName}\}\}`, 'g');
           value = value.replace(regex2, valueToReplace);
-          _.set(obj1Copy, path, value);
+          _.set(obj, path, value);
         }
       }
     }
   });
-  return obj1Copy;
+  return obj;
+}
+
+/**
+ * Replace any variables that appear in an auth flow object
+ * @param {object} obj The object to replace variables
+ * @param {array} stores An array of stores to use for replacing
+ */
+function replaceVariables(obj, stores) {
+  let objCopy = _.cloneDeep(obj);
+  _.each(stores, (store) => {
+    objCopy = replaceFromStore(objCopy, store.store, store.prefix);
+  });
+  return objCopy;
 }
 
 module.exports = replaceVariables;
--- test.js
+++ test.js
@@ -7,9 +7,12 @@ describe('replace variables', () => {
       age: '{{user.age}}'
     };
     const obj2 = {
-      age: 25
+      store: {
+        age: 25
+      },
+      prefix: 'user'
     };
-    const newObj = replaceVariables(obj1, obj2, 'user');
+    const newObj = replaceVariables(obj1, [ obj2 ]);
     expect(newObj).to.have.property('age');
     expect(newObj.age).to.equal('25');
   });
@@ -18,10 +21,13 @@ describe('replace variables', () => {
       age: 'Your age is {{user.age}}. Next year you will be {{user.nextYearAge}}'
     };
     const obj2 = {
-      age: 25,
-      nextYearAge: 26
+      store: {
+        age: 25,
+        nextYearAge: 26
+      },
+      prefix: 'user'
     };
-    const newObj = replaceVariables(obj1, obj2, 'user');
+    const newObj = replaceVariables(obj1, [ obj2 ]);
     expect(newObj).to.have.property('age');
     expect(newObj.age).to.equal('Your age is 25. Next year you will be 26');
   });
@@ -30,11 +36,14 @@ describe('replace variables', () => {
       age: '{{user.obj}}'
     };
     const obj2 = {
-      obj: {
-        born: 1995
-      }
+      store: {
+        obj: {
+          born: 1995
+        }
+      },
+      prefix: 'user'
     };
-    const newObj = replaceVariables(obj1, obj2, 'user');
+    const newObj = replaceVariables(obj1, [ obj2 ]);
     expect(newObj).to.have.property('age');
     expect(newObj.age).to.have.property('born');
     expect(newObj.age.born).to.equal(1995);
